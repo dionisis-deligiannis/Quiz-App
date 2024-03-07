764fef211fe4a49ca1d552f89d7495db41fc6ca1
dionisis-deligiannis <delisdionysios@gmail.com>
2024-03-07 12:19:59 +0200

    final commit

--- index.js
+++ index.js
@@ -63,6 +63,10 @@ function showQuestion(){
         button.classList.add("btn");
         answerButtons.appendChild(button);
         console.log(answer.text)
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click",selectAnswer)
     });
 }
 
@@ -73,4 +77,44 @@ function resetState(){
     }
 }
 
+function selectAnswer(e){
+    const selectedBtn =e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true"
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display ="block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+nextButton.innerHTML = "Play Again"
+nextButton.style.display = "block"
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+nextButton.addEventListener("click",()=>{
+    if(currentQuestionIndex< questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
 startQuiz();
